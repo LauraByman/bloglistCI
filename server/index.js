7858fcbe9a50f9ad29f9b4635f84e65ad4aad253
Laura Byman <laura.a.byman@gmail.com>
2021-02-12 12:38:51 +0200

    testing pipeline

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const logger = require('./utils/logger')
 
 // eslint-disable-next-line no-unused-vars
 const server = http.createServer(app)
-const port = process.env.PORT ? process.env.PORT : 3000;
+const port = process.env.PORT ? process.env.PORT : 3000
 
 app.listen(port, () => {
   logger.info(`Server running on port ${port}`)
